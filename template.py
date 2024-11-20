683906fc56a499e77e69a664cf2a1e13859a72e2
Steve Hill <code@seesteve.xyz>
2024-11-20 11:53:49 +0000

    Update template.py to separate the function for reading input

--- template.py
+++ template.py
@@ -1,5 +1,22 @@
 from pathlib import Path
 
+TEST_INPUT = """\
+test
+your
+input
+here
+"""
+
+
+def read(input):
+    """Read the input from the inputs/ folder"""
+    aoc_dir = Path(__file__).parents[2]
+    event_dir_name = Path(__file__).parent.stem
+    file_name = Path(__file__).stem
+    with open(aoc_dir / "inputs" / event_dir_name / file_name) as f:
+        input = f.read()
+    return input
+
 
 def part_one(input):
     pass
@@ -10,11 +27,8 @@ def part_two(input):
 
 
 if __name__ == "__main__":
-    aoc_dir = Path(__file__).parents[2]
-    event_dir_name = Path(__file__).parent.stem
-    file_name = Path(__file__).stem
-    with open(aoc_dir / "inputs" / event_dir_name / file_name) as f:
-        input = f.read()
+    input = TEST_INPUT.strip()
+    # input = read(input)
 
     print("Part one:", part_one(input))
     print("Part two:", part_two(input))
