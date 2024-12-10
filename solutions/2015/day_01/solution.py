0861ad11892c7a2e02e9e9e96a4cadd2e0898f4a
Steve Hill <code@seesteve.xyz>
2024-12-10 10:44:15 +0000

    Update solution for 2015 day 1
    Change to one combined `solve` function

--- solutions/2015/day_01/solution.py
+++ solutions/2015/day_01/solution.py
@@ -9,15 +9,11 @@ class Solution(TextSolution):
     _year = 2015
     _day = 1
 
-    @answer(280)
-    def part_1(self) -> int:
-        return self.input.count("(") - self.input.count(")")
-
-    @answer(1797)
-    def part_2(self) -> int:
-        floor = 0
-        for step, instruction in enumerate(self.input, start=1):
-            floor += 1 if instruction == "(" else -1 if instruction == ")" else 0
-            if floor == -1:
-                return step
-        raise ValueError("You didn't reach floor -1")
+    @answer((280, 1797))
+    def solve(self) -> tuple[int, int]:
+        floor, steps = 0, 0
+        for step, move in enumerate(self.input, start=1):
+            floor += 1 if move == "(" else -1 if move == ")" else 0
+            if floor == -1 and not steps:
+                steps = step
+        return floor, steps
