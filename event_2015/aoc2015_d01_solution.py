d626e77e0ad21e082aa044b206b539269743e135
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-02-28 07:29:31 +0000

    Add 2015 day 1

---
+++ event_2015/aoc2015_d01_solution.py
@@ -0,0 +1,25 @@
+# Advent of Code 2015 Day 1
+# Not Quite Lisp
+# https://adventofcode.com/2015/day/1
+
+import sys
+
+
+def part_1(input: str) -> int:
+    return input.count("(") - input.count(")")
+
+
+def part_2(input: str) -> int:
+    floor = 0
+    for step, move in enumerate(input, start=1):
+        floor += 1 if move == "(" else -1 if move == ")" else 0
+        if floor == -1:
+            return step
+    return -1
+
+
+if __name__ == "__main__":
+    input: str = sys.stdin.read()
+
+    print("Part 1:", part_1(input))  # 280
+    print("Part 2:", part_2(input))  # 1797
