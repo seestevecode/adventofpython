e0cce0624f68c9690f5779b501da96d86b612874
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-02-28 07:44:04 +0000

    Add 2022 day 2

---
+++ event_2022/aoc2022_d02_solution.py
@@ -0,0 +1,39 @@
+# Advent of Code 2022 Day 2
+# Rock Paper Scissors
+# https://adventofcode.com/2022/day/2
+
+import sys
+
+OUTCOMES = {"win": ("AY", "BZ", "CX"), "draw": ("AX", "BY", "CZ")}
+CHOICES = {"rock": ("AY", "BX", "CZ"), "paper": ("AZ", "BY", "CX")}
+
+
+def parse(input: str) -> list[str]:
+    return [line.replace(" ", "") for line in input.split("\n")]
+
+
+def score(game: str, part: int) -> int:
+    assert len(game) == 2 and game[0] in "ABC" and game[1] in "XYZ", "Invalid game"
+    if part == 1:
+        outcome = 6 if game in OUTCOMES["win"] else 3 if game in OUTCOMES["draw"] else 0
+        choice = 1 if game[1] == "X" else 2 if game[1] == "Y" else 3
+    else:
+        assert part == 2, "Invalid part"
+        outcome = 6 if game[1] == "Z" else 3 if game[1] == "Y" else 0
+        choice = 1 if game in CHOICES["rock"] else 2 if game in CHOICES["paper"] else 3
+    return outcome + choice
+
+
+def part_1(input: str) -> int:
+    return sum(score(game, part=1) for game in parse(input))
+
+
+def part_2(input: str) -> int:
+    return sum(score(game, part=2) for game in parse(input))
+
+
+if __name__ == "__main__":
+    input: str = sys.stdin.read().strip()
+
+    print("Part 1:", part_1(input))  # 11603
+    print("Part 2:", part_2(input))  # 12725
