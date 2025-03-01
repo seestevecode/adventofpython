5c76383e2e7c8d020a37429dea3e7e84695f87a0
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-03-01 14:21:36 +0000

    Add 2015 day 4

---
+++ event_2015/aoc2015_d04_solution.py
@@ -0,0 +1,31 @@
+# Advent of Code 2015 Day 4
+# The Ideal Stocking Stuffer
+# https://adventofcode.com/2015/day/4
+
+import hashlib
+import itertools
+import sys
+
+
+def solve(input: str, prefix: str) -> int:
+    for num in itertools.count():
+        hash_input = f"{input}{num}".encode()
+        hash_result = hashlib.md5(hash_input).hexdigest()
+        if hash_result.startswith(prefix):
+            return num
+    return -1
+
+
+def part_1(input: str) -> int:
+    return solve(input, "00000")
+
+
+def part_2(input: str) -> int:
+    return solve(input, "000000")
+
+
+if __name__ == "__main__":
+    input: str = sys.stdin.read().strip()
+
+    print("Part 1:", part_1(input))  # 117946
+    print("Part 2:", part_2(input))  # 3938038
