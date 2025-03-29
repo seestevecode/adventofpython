8a5b11544f7a5028278e2982edd0eba521f3c6fe
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-03-29 12:41:20 +0000

    Refactor 2020 day 2
    Use unpacking in `valid_count`. Rename anonymous functions. Compact
    `parse`.

--- event_2020/aoc2020_d02_solution.py
+++ event_2020/aoc2020_d02_solution.py
@@ -5,31 +5,25 @@
 import re
 import sys
 
-VALID_CHK1 = lambda min_, max_, char, pword: min_ <= pword.count(char) <= max_
-VALID_CHK2 = lambda p1, p2, char, pwrd: (pwrd[p1 - 1] == char) ^ (pwrd[p2 - 1] == char)
+COUNT_IN_RANGE = lambda low, high, char, pwd: low <= pwd.count(char) <= high
+CHAR_IN_POSITION = lambda a, b, char, pwd: (pwd[a - 1] == char) ^ (pwd[b - 1] == char)
 
 
 def parse(row: str) -> tuple[int, int, str, str]:
-    pattern = r"(\d+)-(\d+) ([a-z]): ([a-z]+)"
-    match = re.match(pattern, row)
-    if match:
+    if match := re.match(r"(\d+)-(\d+) ([a-z]): ([a-z]+)", row):
         return int(match.group(1)), int(match.group(2)), match.group(3), match.group(4)
     raise ValueError("No match in row")
 
 
 def valid_count(input: list[str], valid_check) -> int:
-    return sum(
-        valid_check(a, b, char, pword)
-        for row in input
-        for a, b, char, pword in [parse(row)]
-    )
+    return sum(valid_check(*parse(row)) for row in input)
 
 
 if __name__ == "__main__":
     input: list[str] = sys.stdin.read().strip().split("\n")
 
-    part_1 = valid_count(input, VALID_CHK1)
-    part_2 = valid_count(input, VALID_CHK2)
+    part_1 = valid_count(input, COUNT_IN_RANGE)
+    part_2 = valid_count(input, CHAR_IN_POSITION)
 
     print("Part 1:", part_1)  # 620
     print("Part 2:", part_2)  # 727
