9b2131074b7852f616831bf3358b1e2d68a90c8c
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-04-08 17:13:50 +0100

    Refactor 2015 day 2
    Remove extraneous `parse` function.

--- event_2015/aoc2015_d02_solution.py
+++ event_2015/aoc2015_d02_solution.py
@@ -4,16 +4,12 @@
 
 import sys
 
-
-def parse(input: list[str]) -> list[list[int]]:
-    return [sorted(map(int, line.split("x"))) for line in input]
-
-
 if __name__ == "__main__":
     input: list[str] = sys.stdin.read().split()
+    parsed = [sorted(map(int, line.split("x"))) for line in input]
 
-    part_1: int = sum(2 * (a * b + b * c + c * a) + a * b for a, b, c in parse(input))
-    part_2: int = sum(2 * (a + b) + a * b * c for a, b, c in parse(input))
+    part_1: int = sum(2 * (a * b + b * c + c * a) + a * b for a, b, c in parsed)
+    part_2: int = sum(2 * (a + b) + a * b * c for a, b, c in parsed)
 
     print("Part 1:", part_1)  # 1586300
     print("Part 2:", part_2)  # 3737498
