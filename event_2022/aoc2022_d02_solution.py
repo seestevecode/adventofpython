1f1bc3d50840181b5d2c62ec429f651eb5845676
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-04-10 06:52:49 +0100

    Refactor 2022 Day 2
    Split scoring into separate functions

--- event_2022/aoc2022_d02_solution.py
+++ event_2022/aoc2022_d02_solution.py
@@ -8,24 +8,27 @@ OUTCOMES = {"win": ("AY", "BZ", "CX"), "draw": ("AX", "BY", "CZ")}
 CHOICES = {"rock": ("AY", "BX", "CZ"), "paper": ("AZ", "BY", "CX")}
 
 
-def score(game: str, part: int) -> int:
-    assert len(game) == 2 and game[0] in "ABC" and game[1] in "XYZ", "Invalid game"
-    if part == 1:
-        outcome = 6 if game in OUTCOMES["win"] else 3 if game in OUTCOMES["draw"] else 0
-        choice = 1 if game[1] == "X" else 2 if game[1] == "Y" else 3
-    else:
-        assert part == 2, "Invalid part"
-        outcome = 6 if game[1] == "Z" else 3 if game[1] == "Y" else 0
-        choice = 1 if game in CHOICES["rock"] else 2 if game in CHOICES["paper"] else 3
+def score_1(game: str) -> int:
+    outcome = 6 if game in OUTCOMES["win"] else 3 if game in OUTCOMES["draw"] else 0
+    choice = 1 if game[1] == "X" else 2 if game[1] == "Y" else 3
+    return outcome + choice
+
+
+def score_2(game: str) -> int:
+    outcome = 6 if game[1] == "Z" else 3 if game[1] == "Y" else 0
+    choice = 1 if game in CHOICES["rock"] else 2 if game in CHOICES["paper"] else 3
     return outcome + choice
 
 
 if __name__ == "__main__":
     input: str = sys.stdin.read().strip()
     parsed = [line.replace(" ", "") for line in input.split("\n")]
+    assert all(
+        len(game) == 2 and game[0] in "ABC" and game[1] in "XYZ" for game in parsed
+    ), "Invalid game exists in input"
 
-    part_1: int = sum(score(game, part=1) for game in parsed)
-    part_2: int = sum(score(game, part=2) for game in parsed)
+    part_1: int = sum(score_1(game) for game in parsed)
+    part_2: int = sum(score_2(game) for game in parsed)
 
     print("Part 1:", part_1)  # 11603
     print("Part 2:", part_2)  # 12725
