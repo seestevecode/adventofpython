f6568832c5b83eafc5b4d5e06a181aaf331a580d
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-04-10 06:45:17 +0100

    Refactor 2022 Day 1
    Remove unnecessary function

--- event_2022/aoc2022_d01_solution.py
+++ event_2022/aoc2022_d01_solution.py
@@ -4,17 +4,13 @@
 
 import sys
 
-
-def list_sums(input: list[list[int]]) -> list[int]:
-    return sorted([sum(calorie_list) for calorie_list in input], reverse=True)
-
-
 if __name__ == "__main__":
     input: list[str] = sys.stdin.read().split("\n\n")
     parsed = [[int(item) for item in calorie_list.split()] for calorie_list in input]
+    list_sums = sorted([sum(calorie_list) for calorie_list in parsed], reverse=True)
 
-    part_1 = list_sums(parsed)[0]
-    part_2 = sum(list_sums(parsed)[0:3])
+    part_1 = list_sums[0]
+    part_2 = sum(list_sums[0:3])
 
     print("Part 1:", part_1)  # 70509
     print("Part 2:", part_2)  # 208567
