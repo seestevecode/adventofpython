d04e06a762ec08eadcc2b1cdd8c467e2eca63cf3
See Steve Code <229538+seestevecode@users.noreply.github.com>
2025-04-10 07:38:09 +0100

    Refactor 2015 Day 4
    Rename function to something less generic

--- event_2015/aoc2015_d04_solution.py
+++ event_2015/aoc2015_d04_solution.py
@@ -7,7 +7,7 @@ import itertools
 import sys
 
 
-def solve(input: str, prefix: str) -> int:
+def find_hash(input: str, prefix: str) -> int:
     for num in itertools.count():
         hash_input = f"{input}{num}".encode()
         hash_result = hashlib.md5(hash_input).hexdigest()
@@ -19,8 +19,8 @@ def solve(input: str, prefix: str) -> int:
 if __name__ == "__main__":
     input: str = sys.stdin.read().strip()
 
-    part_1: int = solve(input, "00000")
-    part_2: int = solve(input, "000000")
+    part_1: int = find_hash(input, "00000")
+    part_2: int = find_hash(input, "000000")
 
     print("Part 1:", part_1)  # 117946
     print("Part 2:", part_2)  # 3938038
